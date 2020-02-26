ad51d14fb934b5cab127163b4406b2feb8c2c1cd
alaafar256 <58630587+alaafar256@users.noreply.github.com>
2020-02-26 20:23:24 +0200

    Add files via upload

--- Team14_Git_Assignment/src/Main.java
+++ Team14_Git_Assignment/src/Main.java
@@ -4,7 +4,8 @@ public class Main {
 	private static ISubscriber subscribers [] = {
 			new SimpleSubscriber(),
             new ReallySimpleSubscriber(),
-            new CircleArea(),
+           new CircleArea(),
+            new Fibonacci() , 
 	};
 	public static void main(String[] args) {
 		Topic mathTopic = new Topic();
